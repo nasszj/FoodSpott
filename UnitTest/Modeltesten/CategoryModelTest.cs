cc2f223c5d8520f76cb20deed95bdb7b8c6d1f71
Nasteeho <nasteehojimale@hotmail.com>
2026-04-23 15:15:04 +0200

    Added ProductModelTest and CategoryModelTest.

---
+++ UnitTest/Modeltesten/CategoryModelTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServiceLibrary.Models;
+
+namespace UnitTest.Modeltesten
+{
+    [TestClass]
+    public class CategoryModelTest
+    {
+        [TestMethod]
+        public void Category_Constructot_SetsPropertiesCorrectly()
+        {
+            //Arrange
+            var categoryID = 1;
+            var name = "Pizza";
+
+            //Act
+            var category = new Category(categoryID, name);
+
+            //Assert
+            Assert.AreEqual(categoryID, category.CategoryID);
+            Assert.AreEqual(name, category.Name);
+        }
+    }
+}
---
+++ UnitTest/Modeltesten/ProductModelTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLibrary.Models;
+
+namespace UnitTest.ModelTesten
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void Product_Constructor_SetsPropertiesCorrectly()
+        {
+            //Arrange
+            var productID = 1;
+            var name = "Cheeseburger";
+            var price = 6.00m;
+            var description = "Beef burger with cheese";
+            var categoryID = 2;
+
+            //Act
+            var product = new Product(productID, name, price, description, categoryID);
+
+            //Assert
+            Assert.AreEqual(productID, product.ProductID);
+            Assert.AreEqual(name, product.Name);
+            Assert.AreEqual(price, product.Price);
+            Assert.AreEqual(description, product.Description);
+            Assert.AreEqual(categoryID, product.CategoryID);
+        }
+    }
+}
